9c678e6a790c843fd11fa684e531632c7f1ed3c9
William Brandin <william.brandin@gmail.com>
2018-03-23 15:47:56 -0500

    Revert "File deleted"
    This reverts commit 10486a96f038f38fe98e2e09a866aca31edabcde.

---
+++ SchoolConnectOnBoarding/Database/Models/SCHomeFeature.swift
@@ -0,0 +1,27 @@
+//
+//  SCHomeFeature.swift
+//  SchoolConnectOnBoarding
+//
+//  Created by William Brandin on 3/20/18.
+//  Copyright © 2018 William Brandin. All rights reserved.
+//
+
+import Foundation
+import Firebase
+import RealmSwift
+
+class SCHomeFeature: Object {
+    
+    //MARK: - Properties
+    @objc dynamic var title: String?
+    @objc dynamic var subtitle: String?
+    
+    //MARK: - Init
+    
+    
+    //MARK: - Methods
+    
+    
+}
+
+
