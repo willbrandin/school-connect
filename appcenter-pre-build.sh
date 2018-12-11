9ac02c5ce66668eb607c43a173634249fe06d0a4
willbrandin <will@tidelaundry.com>
2018-12-11 14:26:09 -0600

    testing script

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -3,5 +3,5 @@
 # Example: Change bundle name of an iOS app for non-production
 if [ "$APPCENTER_BRANCH" == "master" ];
 then
-    plutil -replace BuildMode=1 $APPCENTER_SOURCE_DIRECTORY/SchoolConnectOnBoarding/Helper/Info.plist
+    plutil -replace BuildMode -string "\$(PRODUCT_NAME) Beta" $APPCENTER_SOURCE_DIRECTORY/SchoolConnectOnBoarding/Helper/Info.plist
 fi
