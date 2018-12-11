d82173afab9df87b1ae2e7b45aa8f99f82ae821b
willbrandin <will@tidelaundry.com>
2018-12-11 14:19:51 -0600

    remove from ignore

---
+++ appcenter-pre-build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# Example: Change bundle name of an iOS app for non-production
+if [ "$APPCENTER_BRANCH" == "master" ];
+then
+    plutil -replace BuildMode -Int 1 $APPCENTER_SOURCE_DIRECTORY/SchoolConnectOnBoarding/Info.plist
+fi
